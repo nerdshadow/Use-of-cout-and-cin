4c06d43cd3d05c75372068d0e54acb0302ffe0a9
Evilnood <evilnood666@gmail.com>
2021-03-12 10:57:06 +0200

    Добавьте файлы проекта.

---
+++ Use of cout and cin.cpp
@@ -0,0 +1,13 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "What`s your name?" << endl;
+    string Name;
+    cin >> Name;
+    cout << "Welcome to the programming, " << Name;
+
+}
